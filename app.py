cd3d1a074a604f541f2375c8760b21af8a9535d2
Madiha-Ilaf <madihailaf3133@gmail.com>
2022-01-08 09:17:49 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,url_for, redirect, render_template ## importing necessary libraries
+import pickle  ## pickle for loading model(Daibetes.pkl)
+import pandas as pd## to convert the input data into a dataframe for giving as a input to the model
+
+app = Flask(__name__)  ## setting up the flask name
+
+
+model = pickle.load(open("Diabetes.pkl","rb")) ##loading model
+
+@app.route('/')  ## Defining main index route
+def home():
+    return render_template("index.html")  ## showing index.html as homepage
+
+
+@app.route('/predict',methods=['POST','GET']) ## this route will be called when predict button is called
+def predict():
+    text1 = request.form['1'] ## Fetching each input feild value one by one
+    text2 = request.form['2']
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])    ## creating a data frame using all the values
+    print(row_df)
+    prediction = model.predict_proba(row_df)  ## predicting tha output
+    output = '{0:.{1}f}'.format(prediction[0][1] , 2)   ## fromatting output
+
+    if output>str(0.5):
+        return render_template('index.html',pred = 'You have chance of having diabetes.\nProbability of having Diabetes is {}'.format(output))   ## Returning the message for use on the same index.html page
+    else:
+        return render_template('index.html',pred = 'You are safe.\n Probability of having diabetes is {}'.format(output))
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)   ## Running the app as debug==True
+    
